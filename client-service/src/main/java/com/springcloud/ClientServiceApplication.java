9190b41e2a15839e9aede55a0f8a86b06cbbcf16
JKLeGend <zhangjukai@gmail.com>
2020-12-26 14:29:59 +0800

    add prometheus; * intergrate eureka and prometheus with eureka-consul-adapter; * eureka-consul-adapter version is important, wrong version will lead to client-service 404 error in prometheus server; * pay attentions to host and docker inside connection;

--- client-service/src/main/java/com/springcloud/ClientServiceApplication.java
+++ client-service/src/main/java/com/springcloud/ClientServiceApplication.java
@@ -1,7 +1,11 @@
 package com.springcloud;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ClientServiceApplication {
@@ -10,4 +14,9 @@ public class ClientServiceApplication {
         SpringApplication.run(ClientServiceApplication.class, args);
     }
 
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> configurer(
+            @Value("${spring.application.name}") String applicationName) {
+        return (registry) -> registry.config().commonTags("application", applicationName);
+    }
 }
